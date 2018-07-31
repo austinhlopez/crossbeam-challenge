b1f1bad99d18dc25c7cb2ebdb36cea005bec5293
Austin Lopez <austinhlopez@gmail.com>
2018-07-31 12:42:46 -0400

    Add some comments for clarity

--- app/crossbeam/challenge/lib/challenge.ex
+++ app/crossbeam/challenge/lib/challenge.ex
@@ -1,24 +1,29 @@
 defmodule Challenge do
   def main(datasets) do
-    HTTPoison.start()    
-    maps = datasets |>
-      Enum.map(fn dataset ->	
-	url = "https://s3.amazonaws.com/challenge.getcrossbeam.com/public/" <> dataset <> ".json"
+    HTTPoison.start()
+    # map over list of datasets
+    maps = Enum.map(datasets,
+      fn dataset ->	
+p	url = "https://s3.amazonaws.com/challenge.getcrossbeam.com/public/" <> dataset <> ".json"
 
 	response = HTTPoison.get!(url)
-	obj = response.body |> Poison.decode!
 
+	# extract domains
+	obj = response.body |> Poison.decode!
 	company_map = obj["companies"] |>
 	  Enum.reduce(MapSet.new(), fn company, mapset ->
 	    mapset |> MapSet.put(company["domain"])
 	  end)
 
+	# write unique domain count
 	IO.write(Integer.to_string(MapSet.size(company_map)) <> " ")
 
 	company_map
-      end)
+      end
+    )
 
     [first, second] = maps
+    # print the unique domains overlapping
     IO.puts MapSet.size(MapSet.intersection(first, second))
   end
 end
